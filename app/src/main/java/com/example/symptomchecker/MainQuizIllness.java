cd45098f3779a1344eaeed21fc326d2e5f22a090
oussamanech <oussamanach75@gmail.com>
2022-05-20 19:59:33 +0300

    Update MainQuizOneIllnessCursorAdapter

--- app/src/main/java/com/example/symptomchecker/MainQuizIllness.java
+++ app/src/main/java/com/example/symptomchecker/MainQuizIllness.java
@@ -2,9 +2,23 @@ package com.example.symptomchecker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.LoaderManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
-public class MainQuizIllness extends AppCompatActivity {
+import com.example.symptomchecker.data.IllnessContracr.IllnessEntry;
+
+public class MainQuizIllness extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    /**
+     * Identifier for the --- data loader
+     */
+    private static final int LOADER = 0;
+    private static final int LOADER_EXIST = 1;
+
+    private Uri mSelectIllnessUri = IllnessEntry.CONTENT_URI;
+    SymptomCursorAdapter mSymptomCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/symptomchecker/MainQuizOneIllnessCursorAdapter.java
+++ app/src/main/java/com/example/symptomchecker/MainQuizOneIllnessCursorAdapter.java
@@ -1,4 +1,40 @@
 package com.example.symptomchecker;
 
-public class MainQuizOneIllnessCursorAdapter {
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.symptomchecker.data.IllnessContracr.IllnessEntry;
+
+public class MainQuizOneIllnessCursorAdapter extends CursorAdapter {
+
+    public MainQuizOneIllnessCursorAdapter (Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return LayoutInflater.from(context).inflate(R.layout.list_search_illnesses, viewGroup, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        // Find individual views that we want to modify in the list item layout
+        TextView name_illnessTextView = (TextView) view.findViewById(R.id.TextView_name_search_illness);
+
+        // Find the columns of hosp attributes that we're interested in
+        int nameColumnIndex = cursor.getColumnIndex(IllnessEntry.COLUMN_ILLNESS_NAME);
+
+        // Read the hosp attributes from the Cursor for the current hos
+        String a1 = cursor.getString(nameColumnIndex);
+
+        name_illnessTextView.setText(a1);
+
+    }
+
 }
